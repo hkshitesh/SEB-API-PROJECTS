e1350f26c76798ecbb002fd605fc19f002808cf1
Hitesh <demo@gmail.com>
2026-02-05 00:47:00 +0530

    final commit

---
+++ simpleapi2/src/main/java/com/example/simpleapi/Simpleapi2Application.java
@@ -0,0 +1,13 @@
+package com.example.simpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simpleapi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Simpleapi2Application.class, args);
+    }
+
+}
